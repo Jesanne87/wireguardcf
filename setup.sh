066696da3ddadfaf57b302129d1e1b0b7c6f4141
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-05-22 17:21:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 clear
 cd /usr/bin
 echo -e "${GB}[ INFO ]${NC} ${YB}Downloading Scripts${NC}"
-wget -q -O wayar "https://raw.githubusercontent.com/${GitUser}/wireguardcf/main/wayar"
-wget -q -O wgcf "https://raw.githubusercontent.com/${GitUser}/wireguardcf/main/wgcf"
+wget "https://raw.githubusercontent.com/${GitUser}/wireguardcf/main/wayar"
+wget "https://raw.githubusercontent.com/${GitUser}/wireguardcf/main/wgcf"
 chmod +x wayar
 chmod +x wgcf
 echo -e "${GB}[ INFO ]${NC} ${YB}Installations Done${NC}"
