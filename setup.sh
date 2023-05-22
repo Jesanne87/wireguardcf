cd261cb9016df39cb62ce6662765952edc5271c6
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-05-22 17:19:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ wget -q -O wgcf "https://raw.githubusercontent.com/${GitUser}/wireguardcf/main/w
 chmod +x wayar
 chmod +x wgcf
 echo -e "${GB}[ INFO ]${NC} ${YB}Installations Done${NC}"
+rm -f setup.sh
 clear
